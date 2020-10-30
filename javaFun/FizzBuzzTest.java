8c66fe01a7cae7f5ed0c34c5735141d8918bc90a
Monica Hong <momo.star@gmail.com>
2020-10-30 04:27:48 -0700

    java assignments fundamentals

---
+++ javaFun/FizzBuzzTest.java
@@ -0,0 +1,7 @@
+public class FizzBuzzTest {
+    public static void main(String[] args) {
+        FizzBuzz var = new FizzBuzz();
+        String yes = var.fizzBuzz(15);
+        System.out.println(yes);
+    }
+}
---
+++ javaFun/StringManipulator.java
@@ -0,0 +1,18 @@
+public class StringManipulator {
+    public String trimAndConcat(String str1, String str2) {
+        return str1.trim().concat(str2.trim());
+        
+    }
+
+    public Integer getIndexOrNull(String str1, char letter) {
+        return str1.indexOf(letter);
+    }
+
+    public Integer getIndexOrNull(String str1, String str2) {
+        return str1.indexOf(str2);
+    }
+
+    public String concatSubstring(String str1, int num1, int num2, String str2) {
+        return str1.substring(num1, num2).concat(str2);
+    }
+}
