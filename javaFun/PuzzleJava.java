23cf6fa82a8ec01d38db891d38261bf73a4d250e
Monica Hong <momo.star@gmail.com>
2020-11-14 11:28:08 -0800

    puzzleJava done freaking finally

--- javaFun/PuzzleJava.java
+++ javaFun/PuzzleJava.java
@@ -1,4 +1,6 @@
+import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Collections;
 import java.util.Random;
 public class PuzzleJava {
@@ -13,5 +15,63 @@ public class PuzzleJava {
             }
         }
         return myArray;
+    }
+    public ArrayList<String> shuffleArray(String[] array) {
+        ArrayList<String>  myArray = new ArrayList<String>();
+        List<String> listString = Arrays.asList(array);
+        Collections.shuffle(listString);
+        for(int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+            if(array[i].length() > 5) {
+                myArray.add(array[i]);
+            }
+        }
+        return myArray;
+    }
+    public char [] shuffleAlphabet(String letters) {
+        char[] alphaArray = letters.toCharArray();
+        List<Character> alphaList = new ArrayList<>();
+        for (char ch: alphaArray) {
+            alphaList.add(ch);
+        }
+        Collections.shuffle(alphaList);
+        System.out.println(alphaList);
+        alphaArray = alphaList.toString().toCharArray();
+        System.out.println("printing the last item: " + alphaArray[alphaArray.length - 2]);
+        System.out.println("printing the first item: " + alphaArray[1]);
+        if(alphaArray[1] == 'a' || alphaArray[1] == 'e' || alphaArray[1] == 'i' || alphaArray[1] == 'o' || alphaArray[0] == 'u') {
+            System.out.println("here's a message");
+        }
+        return alphaArray;
+    }
+    public int[] randGen() {
+        int[] intArray = new int[10];
+        Random rand = new Random();
+        for(int i = 0; i < intArray.length; i++) {
+            intArray[i] = rand.nextInt((100 - 55) + 1) + 55;
+        }
+        return intArray;
+    }
+    public int[] sortedNums() {
+        int[] intArray = randGen();
+        Arrays.sort(intArray);
+        System.out.println(intArray);
+        return intArray;
+    }
+    public String randomString(int len) {
+        String chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()";
+        Random rand = new Random();
+        StringBuilder sb = new StringBuilder(len);
+        for(int i = 0; i < len; i++) {
+            sb.append(chars.charAt(rand.nextInt(chars.length())));
+        }
+        return sb.toString();
+    }
+    public String[] makeTen() {
+        String[] strArr = new String[10];
+        for(int i = 0; i < strArr.length; i++) {
+            strArr[i] = randomString(5);
+        }
+        return strArr;
     }
 }
--- javaFun/PuzzleJavaTest.java
+++ javaFun/PuzzleJavaTest.java
@@ -1,8 +1,54 @@
 public class PuzzleJavaTest {
     public static void main(String[] args) {
+        // [x] Create an array with the following values: 3,5,1,2,7,9,8,13,25,32. Print the sum of all numbers in the array. Also have the function return an array that only includes numbers that are greater than 10 (e.g. when you pass the array above, it should return an array with the values of 13,25,32)
         PuzzleJava test1 = new PuzzleJava();
         int[] myArray = {3, 5, 1, 2, 7, 9, 8, 13, 25, 32};
         test1.sumAllNumbers(myArray);
         System.out.println(test1);
+
+        // [x] Create an array with the following values: Nancy, Jinichi, Fujibayashi, Momochi, Ishikawa. Shuffle the array and print the name of each person. Have the method also return an array with names that are longer than 5 characters.
+        PuzzleJava test2 = new PuzzleJava();
+        String[] myArray2 = {"Nancy", "Jinichi", "Fujibayashi", "Momochi", "Ishikawa"};
+        test2.shuffleArray(myArray2);
+        System.out.println(test2);
+
+        // [x] Create an array that contains all 26 letters of the alphabet (this array must have 26 values). Shuffle the array and, after shuffling, display the last letter from the array. Have it also display the first letter of the array. If the first letter in the array is a vowel, have it display a message.
+
+        // To shuffle a collection, you can use the shuffle method of the Collections class.
+        PuzzleJava test3 = new  PuzzleJava();
+        String alphabets = "abcdefghijklmnopqrstuvwxyz";
+        char[] returned = test3.shuffleAlphabet(alphabets);
+        System.out.println("printing what got returned");
+        for(int i = 0; i < returned.length; i++) {
+            System.out.println(returned[i]);
+        }
+        // [x] Generate and return an array with 10 random numbers between 55-100.
+        PuzzleJava test4 = new PuzzleJava();
+        int[] randomNums = test4.randGen();
+        System.out.println(randomNums);
+        for(int i = 0; i < randomNums.length; i++) {
+            System.out.println(randomNums[i]);
+        }
+        // To get a random integer, you can use the nextInt method of the Random class.
+        // [x] Generate and return an array with 10 random numbers between 55-100 and have it be sorted (showing the smallest number in the beginning). Display all the numbers in the array. Next, display the minimum value in the array as well as the maximum value.
+        PuzzleJava test5 = new PuzzleJava();
+        int[] randToSort = test5.sortedNums();
+        for(int i = 0; i < randToSort.length; i++) {
+            System.out.println(randToSort[i]);
+        }
+        System.out.println("printing min: " + randToSort[0]);
+        System.out.println("printing max: " + randToSort[randToSort.length - 1]);
+        // To sort a collection, you can use the sort method of the Collections class.
+
+        // [x] Create a random string that is 5 characters long.
+        PuzzleJava test6 = new PuzzleJava();
+        String gotcha = test6.randomString(5);
+        System.out.println(gotcha);
+        // [x] Generate an array with 10 random strings that are each 5 characters long
+        PuzzleJava test7 = new PuzzleJava();
+        String[] theseStrings = test7.makeTen();
+        for(int i = 0; i < theseStrings.length; i++) {
+            System.out.println(theseStrings[i]);
+        }
     }
 }
