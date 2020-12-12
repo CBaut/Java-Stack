c129d7d9235f4a79111ba1f9916fa15f0db97961
Monica Hong <momo.star@gmail.com>
2020-12-12 05:06:01 -0800

    zoo keeper part 2

--- javaOOP/Spring/src/com/codingdojo/javaoop/zookeeper/Bat.java
+++ javaOOP/Spring/src/com/codingdojo/javaoop/zookeeper/Bat.java
@@ -3,9 +3,25 @@ package com.codingdojo.javaoop.zookeeper;
 public class Bat extends Mammal {
 
 	public Bat() {
-	}
 //	energyLevel 300
+		this.setEnergy(300);
+	}
 //	fly() - print sound and decrease energy by 50
+	public void fly() {
+		System.out.println("VOOSH!");
+		this.updateEnergy(-50);
+		this.displayEnergy();
+	}
 //	eatHumans() - print... nvm.. increase energy by 25
+	public void eatHumans() {
+		System.out.println("OM NOM NOM NOM!");
+		this.updateEnergy(25);
+		this.displayEnergy();
+	}
 //	attackTown() - print sound of town on fire and decrease energy by 100
+	public void attackTown() {
+		System.out.println("AHHHHHHHHHHHH");
+		this.updateEnergy(-100);
+		this.displayEnergy();
+	}
 }
--- javaOOP/Spring/src/com/codingdojo/javaoop/zookeeper/BatTest.java
+++ javaOOP/Spring/src/com/codingdojo/javaoop/zookeeper/BatTest.java
@@ -5,9 +5,18 @@ public class BatTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 //		instantiate Bat
+		Bat ahh = new Bat();
 //		attack 3 towns
+		ahh.attackTown();
+		ahh.attackTown();
+		ahh.attackTown();
 //		eat 2 humans
+		ahh.eatHumans();
+		ahh.eatHumans();
 //		fly twice
+		ahh.fly();
+		ahh.fly();
+		ahh.displayEnergy();
 	}
 
 }
