250e4746a3ec12f728c84d5ea20ff04216d53133
Monica Hong <momo.star@gmail.com>
2020-12-20 08:13:37 -0800

    routing practice

---
+++ javaSpring/springFundamentals/firstproject/src/main/java/com/codingdojo/firstproject/HomeController.java
@@ -0,0 +1,22 @@
+package com.codingdojo.firstproject;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//1. Annotation
+@RestController
+//  class level annotations
+@RequestMapping("/hello")
+public class HomeController {
+//	1. Annotation
+	@RequestMapping("")
+//	3. Method that maps to the request route above
+	public String hello() { // 3
+		return "Hello World!";
+	}
+	
+	@RequestMapping("/world")
+	public String world() {
+		return "Class level annotations are cool too!";
+	}
+}
