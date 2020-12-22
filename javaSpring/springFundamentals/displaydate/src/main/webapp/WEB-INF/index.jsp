52a90664c1e3c0f06a9af07828a246b4952bb67c
Monica Hong <momo.star@gmail.com>
2020-12-22 05:16:03 -0800

    created demo project

---
+++ javaSpring/springFundamentals/displaydate/src/main/webapp/WEB-INF/index.jsp
@@ -0,0 +1,13 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body>
+    <h1>This should display two links Time and Date</h1>
+  </body>
+</html>
--- javaSpring/springFundamentals/hellohuman/src/main/java/com/codingdojo/hellohuman/hellohuman/HellohumanController.java
+++ javaSpring/springFundamentals/hellohuman/src/main/java/com/codingdojo/hellohuman/hellohuman/HellohumanController.java
@@ -1,24 +1,29 @@
 package com.codingdojo.hellohuman.hellohuman;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.web.bind.annotation.RequestParam;
 
-@RestController
+@Controller
 public class HellohumanController {
+    // @RequestMapping("/")
+    // public String index(@RequestParam(value = "firstName", required = false)
+    // String firstName,
+    // @RequestParam(value = "lastName", required = false) String lastName) {
+    // String getName;
+    // String message1 = "<h1>Hello ";
+    // String message2 = "!</h1><p>Welcome to SpringBoot!</p>";
+    // if (firstName == null) {
+    // getName = "Human";
+    // } else if (lastName == null) {
+    // getName = firstName;
+    // } else {
+    // getName = firstName + " " + lastName;
+    // }
+    // return message1 + getName + message2;
+    // }
     @RequestMapping("/")
-    public String index(@RequestParam(value = "firstName", required = false) String firstName,
-            @RequestParam(value = "lastName", required = false) String lastName) {
-        String getName;
-        String message1 = "<h1>Hello ";
-        String message2 = "!</h1><p>Welcome to SpringBoot!</p>";
-        if (firstName == null) {
-            getName = "Human";
-        } else if (lastName == null) {
-            getName = firstName;
-        } else {
-            getName = firstName + " " + lastName;
-        }
-        return message1 + getName + message2;
+    public String index() {
+        return "index.jsp";
     }
 }
